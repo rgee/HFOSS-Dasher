01f1260e02f470a1d3b88840851b55867a1dcdab
Ryan Gee <rgee@wesleyan.edu>
2010-06-30 14:25:22 -0400

    Pulled out testing branch. All tests will be done here. This is in order to add silly things like getters for private data for use in test fixtures. Changes to code that should not be part of production, but are necessary for testing.

---
+++ Testing/dasher_tests/FileWordGenTest.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"   
+#include "FileWordGenerator.h"
+using namespace Dasher;
+
+class FileWordGenTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    gen = new CFileWordGenerator("TestData/file_loader_test_data.txt");
+  }
+  
+  CFileWordGenerator* gen;
+};
+
+
+TEST_F(FileWordGenTest, OpensFileCorrectly) {
+}
