4437de7b166cf979b2bd56c651b184ec92b8315d
japplebaum <julianapplebaum@gmail.com>
2010-07-07 16:11:36 -0400

    Writing more unit tests. Found a bug with recursive calls to EventHandler:: InsertEvent - trying to figure that out.

--- Testing/dasher_tests/EventTest.cpp
+++ Testing/dasher_tests/EventTest.cpp
@@ -2,32 +2,52 @@
 #include "../../Src/TestPlatform/MockInterfaceBase.h"
 #include "../../Src/TestPlatform/MockSettingsStore.h"
 #include "../../Src/DasherCore/EventHandler.h"
+#include "../../Src/DasherCore/SettingsStore.h"
 
+//A Dasher component that allows tests to easily determine whether
+//or not it was notified of an event which it subscribed to
 class DummyComponent : public Dasher::CDasherComponent {
 	
 	public:
 		
-		bool receivedEvent;
+		int evtCount;
 		
-		DummyComponent(Dasher::CEventHandler* pEventHandler, Dasher::CSettingsStore* pSettingsStore) :
+		DummyComponent(Dasher::CEventHandler* pEventHandler, CSettingsStore* pSettingsStore) :
 					   Dasher::CDasherComponent(pEventHandler, pSettingsStore) {
-			receivedEvent = false;
+			evtCount = 0;
 		}
 		
-		void HandleEvent(Dasher::CEvent *pEvent) {
-			receivedEvent = true;
+		virtual void HandleEvent(Dasher::CEvent *pEvent) {
+			evtCount++;
 		}
 };
 
-class EventTest : public ::Testing::Test {
+//An extension of DummyComponent that fires a CStartEvent every time its HandleEvent is called
+class RecursionComponent : public DummyComponent {
+	
+	public:
+	
+		RecursionComponent(Dasher::CEventHandler* pEventHandler, CSettingsStore* pSettingsStore) :
+					   DummyComponent(pEventHandler, pSettingsStore) {
+		}
+		
+		void HandleEvent(Dasher::CEvent *pEvent) {
+			
+			evtCount++;
+			m_pEventHandler->InsertEvent(new Dasher::CStartEvent());
+		}
+};
+
+//Test fixture - sets up the EventHandler, SettingsStore, and InterfaceBase instances.
+class EventTest : public ::testing::Test {
 	
 	public:
 	
 		EventTest() {
 			
 			interfaceBase = new CMockInterfaceBase();
-			settingsStore = new CMockSettingsStore();
 			evtHandler = new Dasher::CEventHandler(interfaceBase);
+			settingsStore = new CMockSettingsStore(evtHandler);
 		}
 		
 	protected:
@@ -37,10 +57,64 @@ class EventTest : public ::Testing::Test {
 		CMockSettingsStore *settingsStore;
 };
 
-TEST_F(EventTest, BasicHandling) {	
+//very basic test - subscribe 5 components, fire one event, and assert that they all were notified
+/*TEST_F(EventTest, SingleEventBasic) {	
  
-	DummyComponent *listener = new DummyComponent(evtHandler, settingsStore);
-	evtHandler->RegisterListener(listener, 1);
+	DummyComponent *listener1 = new DummyComponent(evtHandler, settingsStore);
+	DummyComponent *listener2 = new DummyComponent(evtHandler, settingsStore);
+	DummyComponent *listener3 = new DummyComponent(evtHandler, settingsStore);
+	DummyComponent *listener4 = new DummyComponent(evtHandler, settingsStore);
+	DummyComponent *listener5 = new DummyComponent(evtHandler, settingsStore);
+	
+	evtHandler->RegisterListener(listener1, EV_PARAM_NOTIFY);
+	evtHandler->RegisterListener(listener2, EV_PARAM_NOTIFY);
+	evtHandler->RegisterListener(listener3, EV_PARAM_NOTIFY);
+	evtHandler->RegisterListener(listener4, EV_PARAM_NOTIFY);
+	evtHandler->RegisterListener(listener5, EV_PARAM_NOTIFY);
+	
 	evtHandler->InsertEvent(new CParameterNotificationEvent(12));
-	ASSERT_TRUE(listener->receievedEvent);
+	
+	ASSERT_EQ(listener1->evtCount, 1);
+	ASSERT_EQ(listener2->evtCount, 1);
+	ASSERT_EQ(listener3->evtCount, 1);
+	ASSERT_EQ(listener4->evtCount, 1);
+	ASSERT_EQ(listener5->evtCount, 1);
+}
+
+//stress test - register a lot of listeners, fire a lot of events, and assert that 
+//everything was notified without crashing
+TEST_F(EventTest, StressTestBasic) {
+	
+	std::vector<DummyComponent*> components;
+	
+	for(int c = 0; c < 10000; c++) {
+		
+		DummyComponent *newComponent = new DummyComponent(evtHandler, settingsStore);
+		evtHandler->RegisterListener(newComponent, EV_PARAM_NOTIFY);
+	}
+	
+	for(int c = 0; c < 10000; c++) {
+		evtHandler->InsertEvent(new CParameterNotificationEvent(0));
+	}
+	
+	for(std::vector<DummyComponent*>::iterator iter = components.begin(); iter != components.end(); ++iter) {
+		ASSERT_EQ((*iter)->evtCount, 10000);
+	}
+}*/
+
+TEST_F(EventTest, RecursionTestBasic) {
+	
+	RecursionComponent *rec1 = new RecursionComponent(evtHandler, settingsStore);
+	RecursionComponent *rec2 = new RecursionComponent(evtHandler, settingsStore);
+	DummyComponent *listener = new DummyComponent(evtHandler, settingsStore);
+	
+	evtHandler->RegisterListener(rec1, EV_PARAM_NOTIFY);
+	evtHandler->RegisterListener(rec2, EV_PARAM_NOTIFY);
+	evtHandler->RegisterListener(listener, EV_START);
+	
+	evtHandler->InsertEvent(new Dasher::CParameterNotificationEvent(0));
+	
+	ASSERT_EQ(rec1->evtCount, 1);
+	ASSERT_EQ(rec2->evtCount, 1);
+	ASSERT_EQ(listener->evtCount, 2);
 }
