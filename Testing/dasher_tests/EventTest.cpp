89bbcb570f5762f48ae0abc1daab863d49cd9b33
japplebaum <julianapplebaum@gmail.com>
2010-07-07 10:30:39 -0400

    Finally got Gtest integrated into our build process. Starting to write some unit tests for the new event system.

--- Testing/dasher_tests/EventTest.cpp
+++ Testing/dasher_tests/EventTest.cpp
@@ -1,6 +1,46 @@
 #include "gtest/gtest.h"
-#include "MockInterfaceBase.h"
+#include "../../Src/TestPlatform/MockInterfaceBase.h"
+#include "../../Src/TestPlatform/MockSettingsStore.h"
+#include "../../Src/DasherCore/EventHandler.h"
 
-TEST(EventTest, simpletest) {
- EXPECT_EQ(1, 1);
+class DummyComponent : public Dasher::CDasherComponent {
+	
+	public:
+		
+		bool receivedEvent;
+		
+		DummyComponent(Dasher::CEventHandler* pEventHandler, Dasher::CSettingsStore* pSettingsStore) :
+					   Dasher::CDasherComponent(pEventHandler, pSettingsStore) {
+			receivedEvent = false;
+		}
+		
+		void HandleEvent(Dasher::CEvent *pEvent) {
+			receivedEvent = true;
+		}
+};
+
+class EventTest : public ::Testing::Test {
+	
+	public:
+	
+		EventTest() {
+			
+			interfaceBase = new CMockInterfaceBase();
+			settingsStore = new CMockSettingsStore();
+			evtHandler = new Dasher::CEventHandler(interfaceBase);
+		}
+		
+	protected:
+	
+		Dasher::CEventHandler *evtHandler;
+		CMockInterfaceBase *interfaceBase;
+		CMockSettingsStore *settingsStore;
+};
+
+TEST_F(EventTest, BasicHandling) {	
+ 
+	DummyComponent *listener = new DummyComponent(evtHandler, settingsStore);
+	evtHandler->RegisterListener(listener, 1);
+	evtHandler->InsertEvent(new CParameterNotificationEvent(12));
+	ASSERT_TRUE(listener->receievedEvent);
 }
